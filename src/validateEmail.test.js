f56d6ae8f0e882b30679f714a51c6c037c45db4e
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2025-05-22 21:35:18 +0200

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,48 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('test838@gmail.com.');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+    expect(validateEmail('test838@gmail.com.')).toBeTruthy();
   });
 
-  // write more tests here
+  it('should return error when email does not contain @', () => {
+    const result = validateEmail('testamail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return error when email starts with a dot', () => {
+    const result = validateEmail('.test@mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return error when top-level domain starts with a dot', () => {
+    const result = validateEmail('test@.mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return error when email contains double dots', () => {
+    const result = validateEmail('te:st@mail.com');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return 422 if email contains forbidden characters', () => {
+    const result = validateEmail('тест@exam ple.com', 'P@ssword1!');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return error when email starts or ends with a dot', () => {
+    const result = validateEmail('.test@mail.com.');
+
+    expect(result).toBe(false);
+  });
 });
